12682d7fd0cda7d8b7c512db8936646c0f3a04c2
Alexandr Stavschi <alexandr.stavschi@asp.gov.md>
2022-04-29 11:41:50 +0300

    add controller post

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 
 const router = new Router()
 const {Post} = require('./models/models')
+const PostController = require('./controllers/postController')
 
 router.get('/posts', async (req, res) => {
     try {
@@ -12,17 +13,7 @@ router.get('/posts', async (req, res) => {
     }
 });
 
-router.post('/posts', async (req, res) => {
-        console.log(req.body)
-        try {
-            const {author, title, content, picture} = req.body;
-            const post = await Post.create({author, title, content, picture})            
-            await res.json(post);            
-        } catch (error) {
-            console.log(error)
-            res.status(500).json(error)        
-        }
-    });
+router.post('/posts', PostController.create);
 
 router.get('/posts/:id', async (req, res) => {
     try {
